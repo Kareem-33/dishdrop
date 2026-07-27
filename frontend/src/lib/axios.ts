a432c9d6328c8530d836245f962ad3e07d3b9302
Kareem-33 <karemrefaat33@gmail.com>
2026-07-27 22:55:52 +0300

    fix: update API_URL to include versioning in the base URL for axios instance

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "https://dishdrop-api.vercel.app/";
+const API_URL = import.meta.env.VITE_API_URL || "https://dishdrop-api.vercel.app/api/v1";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
