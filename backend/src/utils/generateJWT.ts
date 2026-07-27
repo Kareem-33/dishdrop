502f5eee4ed9edd42a1225baf17cf3d87e708bfd
Kareem-33 <karemrefaat33@gmail.com>
2026-07-28 00:07:31 +0300

    fix: update cookie settings for JWT to enhance security

--- backend/src/utils/generateJWT.ts
+++ backend/src/utils/generateJWT.ts
@@ -12,9 +12,9 @@ export const generateJWT = (payload: object, res: Response) => {
 
   res.cookie('token', token, {
     httpOnly: true,
+    secure: true,
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    sameSite: 'strict',
-    secure: process.env.NODE_ENV === 'production',
   });
 
   return token;
